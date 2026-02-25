bbcace9ab6f74850987f543b598d07f6aba04ad5
João Pedro Dante <jpdante@logtrac.com.br>
2026-02-25 02:08:08 -0300

    Apply migration on start

--- MyWatchState/Program.cs
+++ MyWatchState/Program.cs
@@ -9,7 +9,7 @@ using MyWatchState.Infrastructure.Services;
 namespace MyWatchState;
 
 public class Program {
-  public static void Main(string[] args) {
+  public static async Task Main(string[] args) {
     var builder = WebApplication.CreateBuilder(args);
 
     var keycloak = builder.Configuration
@@ -117,6 +117,11 @@ public class Program {
     app.UseAuthentication();
     app.UseAuthorization();
     app.MapControllers();
+
+    using (var scope = app.Services.CreateScope()) {
+      var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+      await db.Database.MigrateAsync();
+    }
 
     app.Run();
   }
